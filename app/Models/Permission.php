d8f1164a0eb153b1ab50c6d6ceb548c0b0d4b20f
Muhamad Reza  Abdul Rohim <reza.wikrama3@gmail.com>
2018-02-12 01:57:31 +0700

    update

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -2,36 +2,38 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
-use App\Models\MenuMethod;
 use Admin;
+use Illuminate\Database\Eloquent\Model;
 
 class Permission extends Model
 {
-    public $fillable = ['code','menu_method_id'];
+    public $fillable = ['code', 'menu_method_id'];
 
-    public function roleHasPermissionThisMethod($menu="",$method="")
+    public function roleHasPermissionThisMethod($menu = "", $method = "")
     {
-    	$menu = Admin::getMenu($menu);
-    	// $method=Admin::getMethod($method);
-    	$method = $menu->methods()->where('method',$method)->first();
-		if(!empty($menu->id) && !empty($method->id))
-    	{
-    		$role=Admin::getUser()->role;
-	    	$menuMethod = $role->menu_methods()
-	    		->where('menu_id',$menu->id)
-	    		->where('method_id',$method->id)
-	    		->first();
-	    	if(!empty($menuMethod->id) || Admin::getUser()->role->code == 'superadmin')
-	    	// if(!empty($menuMethod->id))
-	    	{
-	    		return 'method_found';
-	    	}else{
-	    		return 'method_not_found_on_role';
-	    	}
-	    }else{
-	    		return 'method_not_protected';
-	    }
-	    	
+        $menu   = Admin::getMenu($menu);
+        $result = 'method_found';
+        if (!empty($menu)) {
+            $method = $menu->methods()->where('method', $method)->first();
+            if (!empty($menu->id) && !empty($method->id)) {
+                $role       = Admin::getUser()->role;
+                $menuMethod = $role->menu_methods()
+                    ->where('menu_id', $menu->id)
+                    ->where('method_id', $method->id)
+                    ->first();
+                if (!empty($menuMethod->id) || Admin::getUser()->role->code == 'superadmin')
+                // if(!empty($menuMethod->id))
+                {
+                    $result = 'method_found';
+                } else {
+                    $result = 'method_not_found_on_role';
+                }
+            } else {
+                $result = 'method_not_protected';
+            }
+        }
+
+        return $result;
+
     }
 }
